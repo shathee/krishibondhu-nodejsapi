b762f161df95bd998fd365b6cdb14d2b7e646906
Baki Ruhunnabi <baki.ruhunnabi@2550.engineering>
2022-03-27 03:10:15 +0200

    dependency and db changed

--- config/dbcon.js
+++ config/dbcon.js
@@ -1,22 +1,21 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('test', 'root', 'example', {
-        dialect: 'mariadb',
-        port: 3307,
-        dialectOptions: {
-          // Your mariadb options here
-          connectTimeout: 1000
-        }
-      });
+const sequelize = new Sequelize("kb", "root", "root", {
+  dialect: "postgres",
+  port: 5432,
+  dialectOptions: {
+    // Your mariadb options here
+    connectTimeout: 1000,
+  },
+});
 
 async () => {
-
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
-}
+};
 
 module.exports = sequelize;
